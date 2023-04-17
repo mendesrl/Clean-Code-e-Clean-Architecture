065f9bf42295d7a1d8ad59be86f1bc499aed3d68
mendesrl <ribeiro.larissamendes@gmail.com>
2023-04-17 15:34:50 -0400

    first commit

--- aula01/dist/test/math.test.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-const math_1 = require("../src/math");
-test("Deve somar 2+2", function () {
-    const result = (0, math_1.sum)(2, 2);
-    expect(result).toBe(4);
-});
