30d5c5ce850fe4310faea702511ada9bfdb23f39
mendesrl <ribeiro.larissamendes@gmail.com>
2023-04-24 11:22:38 -0400

    refatorando condicionais

--- aula01_refactoring/src/main.ts
+++ aula01_refactoring/src/main.ts
@@ -8,44 +8,35 @@ function domingo(diaDaSemana: date) {
   return diaDaSemana.getDay() === 0;
 }
 
+function corridaValida(distancia: number) {
+  return distancia != null && distancia != undefined && distancia > 0;
+}
+
+function dataValida(diaDaSemana: date) {
+  return (
+    diaDaSemana != null &&
+    diaDaSemana != undefined &&
+    diaDaSemana.toString() !== "Invalid Date"
+  );
+}
+
 export function calcularRotas(corridas) {
   let tarifa = 0;
   for (const corrida of corridas) {
-    if (
-      corrida.distancia != null &&
-      corrida.distancia != undefined &&
-      typeof corrida.distancia === "number" &&
-      corrida.distancia > 0
-    ) {
-      if (
-        corrida.diaDaSemana != null &&
-        corrida.diaDaSemana != undefined &&
-        corrida.diaDaSemana instanceof Date &&
-        corrida.diaDaSemana.toString() !== "Invalid Date"
-      ) {
-        if (periodoNoturno(corrida.diaDaSemana)) {
-          if (!domingo(corrida.diaDaSemana)) {
-            tarifa += corrida.distancia * 3.9;
-          } else {
-            tarifa += corrida.distancia * 5;
-          }
-        } else {
-          if (domingo(corrida.diaDaSemana)) {
-            tarifa += corrida.distancia * 2.9;
-          } else {
-            tarifa += corrida.distancia * 2.1;
-          }
-        }
-      } else {
-        return -2;
-      }
-    } else {
-      return -1;
+    if (!corridaValida(corrida.distancia)) return -1;
+    if (!dataValida(corrida.diaDaSemana)) return -2;
+    if (periodoNoturno(corrida.diaDaSemana) && !domingo(corrida.diaDaSemana)) {
+      tarifa += corrida.distancia * 3.9;
+    }
+    if (periodoNoturno(corrida.diaDaSemana) && domingo(corrida.diaDaSemana)) {
+      tarifa += corrida.distancia * 5;
+    }
+    if (!periodoNoturno(corrida.diaDaSemana) && domingo(corrida.diaDaSemana)) {
+      tarifa += corrida.distancia * 2.9;
+    }
+    if (!periodoNoturno(corrida.diaDaSemana) && !domingo(corrida.diaDaSemana)) {
+      tarifa += corrida.distancia * 2.1;
     }
   }
-  if (tarifa < 10) {
-    return 10;
-  } else {
-    return tarifa;
-  }
+  return tarifa < 10 ? 10 : tarifa;
 }
