46c85e3ef48dc0554af8a07014bdc849885a4f34
mendesrl <ribeiro.larissamendes@gmail.com>
2023-05-01 21:33:29 -0400

    atualizando com as novas aulas do curso

--- aula02/projeto/test/main.test.ts
+++ aula02/projeto/test/main.test.ts
@@ -1,11 +1,19 @@
 import { validate } from "../src/main"
 
-test('Deve terstar um CPF válido', function() {
-    const isValid = validate("447.118.751-15")
+test.each([
+    "407.302.170-27",
+    "684.053.160-00",
+    "746.971.314-01"
+])('Deve testar um CPF válido %s', function(cpf: string) {
+    const isValid = validate(cpf)
     expect(isValid).toBeTruthy();
 })
 
-test('Deve terstar um CPF inválido', function() {
-    const isValid = validate("44.118.751-15")
-    expect(isValid).toBeFalsy();
-})
+// test('Deve terstar um CPF válido', function() {
+//     const isValid = validate("447.118.751-15")
+//     expect(isValid).toBeTruthy();
+// })
+ test('Deve terstar um CPF inválido %s', function() {
+     const isValid = validate("44.118.751-15")
+     expect(isValid).toBeFalsy();
+ })
